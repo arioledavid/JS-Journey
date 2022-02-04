26b40d8bcac301c638dcb2e630b95447f02c6cd2
arioledavid <davidariole@gmail.com>
2022-02-04 12:55:54 +0100

    First mini API

--- tripcost/server.js
+++ tripcost/server.js
@@ -3,12 +3,10 @@ const mongo = require("mongodb").MongoClient;
 const url = "mongodb://localhost:27017";
 const app = express();
 
-app.use(express);
+app.use(express.json());
 
 app.post("/trip", (req, res) => {
-  console.log(req.body);
   const name = req.body.name;
-  console.log(trips);
   trips.insertOne({ name: name }, (err, result) => {
     if (err) {
       console.error(err);
@@ -20,16 +18,45 @@ app.post("/trip", (req, res) => {
   });
 });
 
-app.get("/trip", (req, res) => {
-  /*  */
+app.get("/trips", (req, res) => {
+  trips.find().toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ err: err });
+      return;
+    }
+    res.status(200).json({ trips: items });
+  });
 });
 
 app.post("/expense", (req, res) => {
-  /* */
+  expenses.insertOne(
+    {
+      trip: req.body.trip,
+      date: req.body.date,
+      category: req.body.category,
+      description: req.body.description,
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({ err: err });
+        return;
+      }
+      res.status(200).json({ ok: true });
+    }
+  );
 });
 
 app.get("/expenses", (req, res) => {
-  /* */
+  expenses.find({ trip: req.body.trip }).toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({ err: err });
+      return;
+    }
+    res.status(200).json({ expenses: items });
+  });
 });
 
 app.listen(3000, () => console.log("Server ready"));
@@ -52,5 +79,3 @@ mongo.connect(
     expenses = db.collection("expenses");
   }
 );
-
-app.use(express.json());
