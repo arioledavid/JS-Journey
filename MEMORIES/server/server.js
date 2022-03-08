963c2f67302f418fde13e8340d1848af6d69a9f7
arioledavid <davidariole@gmail.com>
2022-03-08 10:48:13 +0100

    Frontend update: Form completed

--- MEMORIES/server/server.js
+++ MEMORIES/server/server.js
@@ -7,11 +7,10 @@ import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://memoriesproject:Memories123@cluster0.vx0mi.mongodb.net/Memories?retryWrites=true&w=majority";
